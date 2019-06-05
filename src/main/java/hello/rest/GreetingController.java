23b4481a293e17c4438946b470c2700f7ef0d973
ALU201715865 <ALU201715865@F7-LAB201-1339.uniritter.local>
2019-06-04 21:55:58 -0300

    Inicial

---
+++ src/main/java/hello/rest/GreetingController.java
@@ -0,0 +1,33 @@
+package rest;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    
+//    @RequestMapping("/numero")
+//    public Greeting numero(@RequestParam(value="max", defaultValue="60") String nameNum) {
+//        return (int) (Math.random()* Integer.parseInt(nameNum) + 1);
+//        
+//    }
+    
+    
+//    
+//    @RequestMapping("/numero")
+//    public Greeting numero(@RequestParam(value="max", defaultValue="60") String nameNum) {
+//        return (int) (Math.random()* Integer.parseInt((nameNum) + 1);
+//        
+//    }
+}
